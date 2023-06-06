baffa71750d51c92faeac9d804b66d890503dae9
tamanegi-mogumogu <110730536+tamanegi-mogumogu@users.noreply.github.com>
2023-06-06 22:29:17 +0900

    revert

--- lineage_starqltechn.mk
+++ lineage_starqltechn.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 AOSPK
+# Copyright (C) 2021 lineageK
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,12 +22,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Inherit from starqltechn device
 $(call inherit-product, device/samsung/starqltechn/device.mk)
 
-# Inherit some common aospOS stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common lineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 
 # Device identifier
-PRODUCT_NAME := aosp_starqltechn
+PRODUCT_NAME := lineage_starqltechn
 PRODUCT_DEVICE := starqltechn
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G9600
