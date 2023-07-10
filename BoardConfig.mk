c1cfec2ab30c47030757858cd950ad2ba6df69f3
tamanegi-mogumogu <110730536+tamanegi-mogumogu@users.noreply.github.com>
2023-07-10 21:17:38 +0900

    fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,5 +28,6 @@ TARGET_OTA_ASSERT_DEVICE := starqltechn
 TARGET_KERNEL_CONFIG := starqlte_chn_open_defconfig
 
 # Vendor init
-#TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_samsung_starqltechn
-#TARGET_RECOVERY_DEVICE_MODULES := init_samsung_starqltechn
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_starqltechn
+TARGET_RECOVERY_DEVICE_MODULES := libinit_starqltechn
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
