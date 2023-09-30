d242b7d7139eaaae7efa47ece1ec7c16ac6221d6
tamanegi-mogumogu <nyusuke2008@gmail.com>
2023-09-30 19:24:17 +0900

    starqltechn: revert

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,8 +27,9 @@ TARGET_USES_VULKAN := true
 TARGET_OTA_ASSERT_DEVICE := starqltechn
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineage_starqltechn_defconfig
+TARGET_KERNEL_CONFIG := starqlte_chn_open_defconfig
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_starqltechn
 TARGET_RECOVERY_DEVICE_MODULES := libinit_starqltechn
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
