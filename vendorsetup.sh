d62f8cb00534bf5e5082c16e1f622d3a02235171
tamanegi-mogumogu <nyusuke2008@gmail.com>
2023-10-18 22:30:52 +0900

    starqltechn: Fix some codes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -24,7 +24,7 @@ echo 'Cloning Hardware Samsung [5/7]'
 # Hardware OSS parts for Samsung
 mv hardware/samsung/nfc .
 rm -rf hardware/samsung
-git clone https://github.com/ElixirOS-982x/hardware_samsung -b evo hardware/samsung
+git clone https://github.com/ElixirOS-982x/hardware_samsung -b evo-udc hardware/samsung
 mv nfc hardware/samsung
 
 echo 'Cloning Samsung_Slsi and Linaro BSP repos [6/7]'
