f710c26ff229cd5f380f4c4fd778dfbdcea0d338
tamanegi-mogumogu <nyusuke2008@gmail.com>
2023-11-01 14:49:37 +0900

    Revert

--- vendorsetup.sh
+++
@@ -1,5 +0,0 @@
-# Hardware OSS parts for Samsung
-mv hardware/samsung/nfc .
-rm -rf hardware/samsung
-git clone https://github.com/tamanegi-mogumogu/hardware_samsung -b thirteen hardware/samsung
-mv nfc hardware/samsung
