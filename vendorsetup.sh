f97a5ea1fc34d5b1f2f17bf37c9d6e2cee033c6c
Tqmane845 <110730536+tamanegi-mogumogu@users.noreply.github.com>
2023-12-09 14:50:54 +0900

    Use tqmane's vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -23,7 +23,7 @@ rm -rf vendor/samsung/sdm845-common
 git clone https://github.com/tamanegi-mogumogu/android_vendor_samsung_sdm845-common -b fourteen vendor/samsung/sdm845-common
 
 rm -rf vendor/samsung/starqltechn
-git clone https://github.com/tamanegi-mogumogu/vendor_samsung_starqltechn vendor/samsung/starqltechn
+git clone https://github.com/tqmane/vendor_samsung_starqltechn vendor/samsung/starqltechn
 
 echo 'Cloning Hardware Samsung [5/6]'
 # Hardware OSS parts for Samsung
